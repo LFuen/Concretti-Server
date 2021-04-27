0ba0f6dc525a9f85db4aba6cb066d0600c07d31f
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2021-04-27 18:50:14 -0400

    fixed error

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const { NODE_ENV } = require('./config')
 const validateBearerToken = require('./middleware/bearer-token')
 const errorHandler = require('./middleware/error-handler')
 const authRouter = require('./routes/auth-router')
-const authRouter = require('./routes/orders-router')
-const authRouter = require('./routes/products-router')
+const ordersRouter = require('./routes/orders-router')
+const productsRouter = require('./routes/products-router')
 const app = express();
 
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
@@ -20,8 +20,8 @@ app.use(errorHandler)
 app.use(validateBearerToken);
 
 app.use("/api/auth", authRouter);
-app.use("/api/orders", authRouter);
-app.use("/api/products", authRouter);
+app.use("/api/orders", ordersRouter);
+app.use("/api/products", productsRouter);
 
 
 module.exports = app;
