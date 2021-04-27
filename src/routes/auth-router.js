15cc97ef63c11d777d368fa96da8bfef53769b19
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2021-04-27 19:42:29 -0400

    unable to acquire a connection

--- src/routes/auth-router.js
+++ src/routes/auth-router.js
@@ -5,14 +5,15 @@ const authRouter = express.Router();
 const jsonBodyParser = express.json();
 
 authRouter.post("/login", jsonBodyParser, (req, res, next) => {
-  const { username, password } = req.body;
-  const loginUser = { username, password };
+  const { user_department, user_password } = req.body;
+  const loginUser = { user_department, user_password };
+  console.log(loginUser)
   for (const [key, value] of Object.entries(loginUser))
     if (value == null)
       return res.status(400).json({
         error: `Missing '${key}' in request body`,
       });
-  AuthService.getUser(req.app.get("db"), loginUser.username)
+  AuthService.getUser(req.app.get("db"), loginUser.user_department)
     .then((dbUser) => {
       if (!dbUser)
         return res.status(400).json({
@@ -20,15 +21,15 @@ authRouter.post("/login", jsonBodyParser, (req, res, next) => {
         });
 
       return AuthService.comparePasswords(
-        loginUser.password,
-        dbUser.password
+        loginUser.user_password,
+        dbUser.user_password
       ).then((compareMatch) => {
         if (!compareMatch)
           return res.status(400).json({
             error: "Incorrect user_name or password",
           });
 
-        const sub = dbUser.username;
+        const sub = dbUser.user_department;
         const payload = { user_id: dbUser.id };
         res.send({
           authToken: AuthService.createJWT(sub, payload),
