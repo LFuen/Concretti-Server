999287df8bfcd165f18679887d33cf8ab6ce8d09
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2021-05-17 00:37:44 -0400

    fixing issue

--- src/routes/orders-router.js
+++ src/routes/orders-router.js
@@ -50,7 +50,7 @@ ordersRouter
   .patch((req, res, next) => {
     const db = req.app.get("db");
     const { orderId } = req.params;
-    return OrdersService.deleteSingleOrder(db, orderId).then((order) => res.status(204).json(order));
+    return OrdersService.deleteSingleOrder(db, orderId).then((order) => res.status(200).json(order));
   });
 
 ordersRouter
