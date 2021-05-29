b89384478d140942ddec01ec2b98ebc8862c8c32
Lili Fuentes <lile7886@gmail.com>
2021-05-29 11:10:48 -0400

    added products and colors

--- src/config.js
+++ src/config.js
@@ -2,7 +2,11 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     JWT_SECRET: process.env.JWT_SECRET || 's3cr3tKey',
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://concretti.vercel.app', // http://localhost:3000 / https://concretti.vercel.app
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000', // http://localhost:3000 / https://concretti.vercel.app
     DATABASE_URL: process.env.DATABASE_URL ||
-    "postgres://evkgjlqehkjzmn:fc0c115319adbd2334a3bf8baca2eeceac291bad61e0f89fea6ca07257f367b9@ec2-34-202-54-225.compute-1.amazonaws.com:5432/d8jqla0c3dh1qn",
+    "postgres://lili@localhost/concretti",
 }
+
+
+// postgres://lili@localhost/concretti
+// postgres://evkgjlqehkjzmn:fc0c115319adbd2334a3bf8baca2eeceac291bad61e0f89fea6ca07257f367b9@ec2-34-202-54-225.compute-1.amazonaws.com:5432/d8jqla0c3dh1qn
