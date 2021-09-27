4fad5dc70ad9796710cf1868e7caabe03872c546
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2021-09-27 17:05:10 -0400

    removed migrateandseed from gitignore

---
+++ migrateAndSeed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+psql -U postgres -d concretti -f ./migrations/002.undo.create_orders.sql
+psql -U postgres -d concretti -f ./migrations/001.undo.create_users.sql
+
+psql -U postgres -d concretti -f ./migrations/001.do.create_users.sql
+psql -U postgres -d concretti -f ./migrations/002.do.create_orders.sql
+
+
+psql -U postgres -d concretti -f ./seeds/seed.tables.sql
+
+
+
+
+# to run file type: ./migrateAndSeed.sh in terminal
+# cahnge user name to whatever username you're using for your db role and db name if you created a db 
+# with a name other than 'concretti' 
+# after creation of this file, include it in the gitignore file so it doesn't upload again
